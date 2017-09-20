da14aff84dfb37a730d54c5e441f99d201503f97
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-09-20 10:43:19 +0900

    maybe ok

--- syokudou_client.py
+++ syokudou_client.py
@@ -1,33 +1,32 @@
+#coding: UTF-8
+from tkinter import *
 import socket
 import sys
-import tkinter
-
-root=tkinter.Tk()
-root.title("食堂利用管理システム")
 
 
-message=tkinter.Label()
-message.pack()
-
-def attend(student_id):
-	print(student_id)
-
-student_id_textbox=tkinter.Entry()
-student_id_textbox.bind("<Key-RETURN>",attend)
-student_id_textbox.pack()
-
-root.mainloop()
-
-while True:
+def key(event):
     try:
-        student_id=input()
+        student_id=student_id_textbox.get()
+        if not student_id:
+                return
         sock=socket.socket()
-        sock.connect(("localhost",55555))
+        sock.connect(("192.168.11.8",55555))
         sock.sendall(student_id.encode())
-        print(sock.recv(1024).decode())
+        message_label_text.set(sock.recv(1024).decode())
+        student_id_textbox_text.set("")
     except ConnectionResetError:
         print("接続が切断されました．LANケーブル，ハブの電源を確認して下さい．")
     except BaseException  as ex:       
         #TODO:BaseException必須?
         print(ex+"原因不明の例外です．")
 
+top = Tk()
+message_label_text = StringVar()
+message_label = Label(top,textvariable=message_label_text,font = ("",40))
+message_label.pack(side=TOP,fill=Y,expand=1)
+student_id_textbox_text = StringVar()
+student_id_textbox = Entry(top,bd=5,width = 10,textvariable=student_id_textbox_text)
+student_id_textbox.pack()
+student_id_textbox.bind("<Return>",key)
+
+top.mainloop()
--- syokudou_server.py
+++ syokudou_server.py
@@ -10,23 +10,23 @@ customers=[]
 
 listen_sock=socket.socket()
 listen_sock.bind(("",55555))
-listen_sock.listen()
+listen_sock.listen(1)
 with open("log.csv","a",newline="") as csvfile:
     while True:
         try:
             client_sock,addr=listen_sock.accept()
-            command=client_sock.recv(1024).decode()
-            print(command)
+            student_id=client_sock.recv(1024).decode()
+            print(student_id)
             #csv_writer = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            #csv_writer.writerow([str(date.today()),command])
+            #csv_writer.writerow([str(date.today()),student_id])
 
             #    customers.clear()
             #    client_sock.sendall("利用履歴を削除しました．".encode())
-            if re.match(r"\d{6}$",command):
-                if command in customers:
+            if re.match(r"\d{6}$",student_id):
+                if student_id in customers:
                     client_sock.sendall("多重利用です!!!!!".encode())
                 else:
-                    customers.append(command)
+                    customers.append(student_id)
                     client_sock.sendall(b"ok")
             else:
                 client_sock.sendall("不正な学生証です．".encode())
