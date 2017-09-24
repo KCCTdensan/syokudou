781d8e60168c1b41aeb2fa5594ca1bfbeb997e6a
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-09-24 19:40:36 +0900

    ちゃんとソケットcloseするように．あと，log残すのも

--- syokudou_server.py
+++ syokudou_server.py
@@ -1,4 +1,3 @@
-#TODO:log残すとか統計取るとか
 #TODO:停電対策(耐障害)
 
 import socket
@@ -8,31 +7,25 @@ import datetime
 
 customers=[]
 
-listen_sock=socket.socket()
-listen_sock.bind(("",55555))
-listen_sock.listen(1)
-with open("log.csv","a",newline="") as csvfile:
-    while True:
-        try:
-            client_sock,addr=listen_sock.accept()
-            student_id=client_sock.recv(1024).decode()
-            print(student_id)
-            #csv_writer = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            #csv_writer.writerow([str(date.today()),student_id])
-
-            #    customers.clear()
-            #    client_sock.sendall("利用履歴を削除しました．".encode())
-            if re.match(r"\d{6}$",student_id):
-                if student_id in customers:
-                    client_sock.sendall("多重利用です!!!!!".encode())
+with socket.socket()as listen_sock:
+    listen_sock.bind(("",55555))
+    listen_sock.listen(1)
+    with open("log.csv","a") as csvfile:
+        while True:
+            try:
+                client_sock,addr=listen_sock.accept()
+                student_id=client_sock.recv(1024).decode()
+                csvfile.write(str(datetime.datetime.today())+","+student_id+"\n")
+                print(student_id)
+                if re.match(r"\d{6}$",student_id):
+                    if student_id in customers:
+                        client_sock.sendall("多重利用です!!!!!".encode())
+                    else:
+                        customers.append(student_id)
+                        client_sock.sendall(b"ok")
                 else:
-                    customers.append(student_id)
-                    client_sock.sendall(b"ok")
-            else:
-                client_sock.sendall("不正な学生証です．".encode())
-        except ConnectionResetError:
-            print("接続が切断されました．LANケーブル，ハブの電源を確認して下さい．")
-        except BaseException  as ex:
-            print(ex+"原因不明の例外です．")
-            
-#TODO:listen_sock.close()
+                    client_sock.sendall("不正な学生証です．".encode())
+            except ConnectionResetError:
+                print("接続が切断されました．LANケーブル，ハブの電源を確認して下さい．")
+            except BaseException  as ex:
+                print(ex+"原因不明の例外です．")
