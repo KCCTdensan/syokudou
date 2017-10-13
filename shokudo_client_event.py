7381168e6b8a4ce6304680d4eaaadfd2ecd6cc18
縦椅子鉋 <TateIsuKanna@users.noreply.github.com>
2017-10-13 18:35:54 +0900

    キーコードの取得だけ

--- shokudo_client_event.py
+++ shokudo_client_event.py
@@ -6,7 +6,14 @@ class event():
         self.string=[]
 
     def onEVT_KEY_DOWN(self,evt):
+        keycode=evt.GetKeyCode()
+        if keycode==13:
+            
+            pass
+        else:
+            self.string+=[keycode]
         print("keydown",evt.GetKeyCode())
 
     def onEVT_KEY_UP(self,evt):
+        print(self.string)
         print("keyup",evt.GetKeyCode())
