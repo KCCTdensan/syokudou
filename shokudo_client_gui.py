05e200c171e3dd2c1210379da8e66433b1dc8bfe
kumataro <r116309@g.kobe-kosen.ac.jp>
2017-10-14 13:40:38 +0900

    tkinterからwxPythonに改造

--- shokudo_client_gui.py
+++ shokudo_client_gui.py
@@ -1,26 +1,12 @@
 #coding: UTF-8
 
 import wx
-from shokudo_client_event import event
 
 
 class gui():
     def __init__(self):
-        self.eventprocess=event();
-
-        self.app=wx.App()
-        self.frame=wx.Frame(None)
-
-        self.frame.Maximize()
-        self.frame.SetTitle(u"食堂管理システム")
-
-        self.textbox=wx.TextCtrl(self.frame,wx.ID_ANY)
-
-        self.frame.Bind(wx.EVT_KEY_DOWN,self.eventprocess.onEVT_KEY_DOWN)
-        self.frame.Bind(wx.EVT_KEY_UP,self.eventprocess.onEVT_KEY_UP)
-        
-        self.app.SetTopWindow(self.frame)
-        self.frame.Show(True)
-
-    def mainloop(self):
-        self.app.MainLoop()
+        font=wx.Font(20, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        self.eventprocess.textbox.SetFont(font)
+        sizer=wx.FlexGridSizer(cols=2, vgap=1, hgap=5)
+        sizer.Add(self.eventprocess.textbox,flag=wx.EXPAND)
+        self.frame.SetSizer(sizer)
