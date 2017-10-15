966243317c3c57ae81a129a4eb0540e16d807461
Yuelia35 <r116330@g.kobe-kosen.ac.jp>
2017-10-15 22:49:04 +0900

    一応完成 電源断から復旧するときにserverに使うPC変えたりすると多分うまくいかない あとloadは起動時(多分)に毎回行われます

--- syokudou_server.py
+++ syokudou_server.py
@@ -6,9 +6,32 @@ import csv
 import datetime
 
 customers=[]
+
+def save(n):
+    """
+    save
+    customers.jsonに指定文字列,日付を書き込む
+    それだけ
+    """
+    r = open('customers.json','a')
+    r.write(str(n) + ":" + str(datetime.date.today()) + "\n")
+    r.close()
+def load():
+    """
+    load
+    この関数を呼び出したときと同日にsaveされた記録をすべてcustomerに書き込む
+    それだけ
+    """
+    r = open('customers.json','r')
+    for toCust in  r.readlines():
+        if toCust[7:17] == str(datetime.date.today()):
+            customers.append(toCust[0:6])
+    r.close()
+
 with socket.socket()as listen_sock:
     listen_sock.bind(("",55555))
     listen_sock.listen(1)
+    load()
     with open("log.csv","a") as csvfile:
         while True:
             try:
@@ -21,6 +44,7 @@ with socket.socket()as listen_sock:
                         client_sock.sendall("多重利用です!!!!!".encode())
                     else:
                         customers.append(student_id)
+                        save(student_id)
                         client_sock.sendall(b"ok")
                 else:
                     client_sock.sendall("不正な学生証です．".encode())
@@ -28,12 +52,3 @@ with socket.socket()as listen_sock:
                 print("接続が切断されました．LANケーブル，ハブの電源を確認して下さい．")
             except BaseException  as ex:
                 print(ex+"原因不明の例外です．")
-def save(n):
-    """
-    save
-    customers.txtに指定文字列と半角スペースを書き込む
-    それだけ
-    """
-    r = open('customers.txt','a')
-    r.write(n + " ")
-    r.close()
