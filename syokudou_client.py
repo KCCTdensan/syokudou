43a032fb2ecdf17ea0763d5d4b7bdc5e761ba40e
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-17 01:13:58 +0900

    不要な物削除

--- syokudou_client.py
+++ syokudou_client.py
@@ -1,14 +1,12 @@
-#coding: UTF-8
 from tkinter import *
 import socket
-import sys
 
 def key(event):
     try:
         student_id=student_id_textbox.get()
         if not student_id:
                 return
-        sock=socket.create_connection(("localhost",55555),timeout=3)
+        sock=socket.create_connection(("192.168.11.8",55555),timeout=3)
         sock.sendall(student_id.encode())
         message_label_text.set(sock.recv(1024).decode())
         root.after(2000,lambda:message_label_text.set(""))
@@ -18,8 +16,8 @@ def key(event):
         student_id_textbox_text.set("")
 
 root = Tk()
-#root.attributes("-zoomed", "1") #Linux
-root.state("zoomed") #Windows
+root.attributes("-zoomed", "1") #Linux
+#root.state("zoomed") #Windows
 message_label_text = StringVar()
 message_label = Label(root,textvariable=message_label_text,font = ("",40),wraplength="25c") #TODO:cと書く公式ドキュメント探し途中
 message_label.pack(side=TOP,fill=Y,expand=1)
--- syokudou_server.py
+++ syokudou_server.py
@@ -1,15 +1,12 @@
-#TODO:停電対策(耐障害)
-
 import socket
 import re
-import csv
 import datetime
 
 customers=[]
 
 with socket.socket()as listen_sock:
     listen_sock.bind(("",55555))
-    listen_sock.listen(1)
+    listen_sock.listen()
     with open("log.csv","a") as csvfile:
         while True:
             try:
@@ -25,7 +22,5 @@ with socket.socket()as listen_sock:
                         client_sock.sendall(b"ok")
                 else:
                     client_sock.sendall("不正な学生証です．".encode())
-            except ConnectionResetError:
-                print("接続が切断されました．LANケーブル，ハブの電源を確認して下さい．")
-            except BaseException  as ex:
-                print(ex+"原因不明の例外です．")
+            except BaseException as ex:
+                print(ex)
