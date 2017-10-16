49f1e705a9af539a497cf7d54dbfddcf361cfa38
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-17 00:20:05 +0900

    タイムアウトの正しい処理，2秒経ったら表示を消す，例外処理と表示簡潔化．変数名リファクタリング

--- syokudou_client.py
+++ syokudou_client.py
@@ -3,32 +3,30 @@ from tkinter import *
 import socket
 import sys
 
-
 def key(event):
     try:
         student_id=student_id_textbox.get()
         if not student_id:
                 return
-        sock=socket.socket()
-        sock.connect(("192.168.11.8",55555))
+        sock=socket.create_connection(("localhost",55555),timeout=3)
         sock.sendall(student_id.encode())
         message_label_text.set(sock.recv(1024).decode())
+        root.after(2000,lambda:message_label_text.set(""))
+    except BaseException as ex:       
+        message_label_text.set(ex)
+    finally:
         student_id_textbox_text.set("")
-    except ConnectionResetError:
-        print("接続が切断されました．LANケーブル，ハブの電源を確認して下さい．")
-    except BaseException  as ex:       
-        #TODO:BaseException必須?
-        print(ex+"原因不明の例外です．")
 
-top = Tk()
-top.attributes("-zoomed", "1")
+root = Tk()
+#root.attributes("-zoomed", "1") #Linux
+root.state("zoomed") #Windows
 message_label_text = StringVar()
-message_label = Label(top,textvariable=message_label_text,font = ("",40))
+message_label = Label(root,textvariable=message_label_text,font = ("",40),wraplength="25c") #TODO:cと書く公式ドキュメント探し途中
 message_label.pack(side=TOP,fill=Y,expand=1)
 student_id_textbox_text = StringVar()
-student_id_textbox = Entry(top,bd=5,width = 10,textvariable=student_id_textbox_text)
+student_id_textbox = Entry(root,bd=5,width = 10,textvariable=student_id_textbox_text)
 student_id_textbox.pack()
 student_id_textbox.bind("<Return>",key)
 student_id_textbox.focus_set()
 
-top.mainloop()
+root.mainloop()
